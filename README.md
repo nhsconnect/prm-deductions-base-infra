39874552beb2e1c60bbb7e74b7a0e0537e767ebd
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-11-14 14:27:24 +0000

    Base infrastructure for MHS

---
+++ README.md
@@ -0,0 +1,8 @@
+# Base infrastructure for deductions PRM
+
+Scope of infrastructure which should be in this repository:
+
+> All infrastructure that is not bound to specific environment.
+
+That implies:
+ * ECR repositories for docker images that we produce
---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  profile   = "default"
+  version   = "~> 2.27"
+  region    = "${var.region}"
+}
+
+terraform{
+      backend "s3" {
+        bucket  = "prm-deductions-terraform-state"
+        key     = "base/terraform.tfstate"
+        region  = "eu-west-2"
+        encrypt = true
+    }
+}
+
+module "mhs" {
+    source              = "./modules/mhs/"
+    region = "${var.region}"
+}
