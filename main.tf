aafd38807b8dab06d80f0a89fd397540a8764f2f
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-11 16:36:55 +0000

    [PRMT-731] removed transit VPC
    OpenTest uses a subnet with VPN client instead

--- main.tf
+++ main.tf
@@ -22,14 +22,3 @@ module "dns" {
     source              = "./modules/dns/"
     region = "${var.region}"
 }
-
-module "transit-opentest" {
-    source              = "./modules/transit-opentest/"
-    region = "${var.region}"
-
-    component_name              = "transit-opentest"
-    cidr                        = "10.10.0.0/16"
-    public_subnets              = ["10.10.1.0/24", "10.10.2.0/24"]
-    private_subnets             = ["10.10.101.0/24", "10.10.102.0/24"]
-    azs                         = ["eu-west-2a", "eu-west-2b"]
-}
