9a1e438b3967b9f7ea19c3aa9bd17863146c62a5
Phil Evans <phil.evans@thoughtworks.com>
2020-02-05 11:52:34 +0000

    Add ECR repo for generic component

--- modules/prm/ecr.tf
+++ modules/prm/ecr.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "poc" {
     name = "prm/prm-migration-poc"
 }
+
+resource "aws_ecr_repository" "generic-component-repo" {
+    name = "deductions/generic-component"
+}
