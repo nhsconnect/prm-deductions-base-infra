bed161b77bfd4d5fda0b88af3f26a433b76ed89e
aiwatko <oktawia.kata1@nhs.net>
2021-04-26 16:12:22 +0100

    [PRMT-1731] Created a public non-prod DNS zone

--- modules/dns/public_zone.tf
+++ modules/dns/public_zone.tf
@@ -20,3 +20,30 @@ resource "aws_ssm_parameter" "public_zone_id" {
 output "public_zone_ns" {
   value = aws_route53_zone.public.name_servers
 }
+
+resource "aws_route53_zone" "non_prod_public" {
+  name = "non-prod.patient-deductions.nhs.uk"
+
+  tags = {
+    CreatedBy = var.repo_name
+  }
+}
+
+resource "aws_route53_record" "non_prod_ns" {
+  name = "non-prod.patient-deductions.nhs.uk"
+  ttl = 30
+  type = "NS"
+  zone_id = aws_route53_zone.public.zone_id
+
+  records = aws_route53_zone.non_prod_public.name_servers
+}
+
+resource "aws_ssm_parameter" "non_prod_public_zone_id" {
+  name = "/repo/output/${var.repo_name}/non-prod-public-zone-id"
+  type  = "String"
+  value = aws_route53_zone.non_prod_public.zone_id
+
+  tags = {
+    CreatedBy = var.repo_name
+  }
+}
